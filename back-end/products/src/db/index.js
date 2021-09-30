444da3312def322866d634a898bab05bb4612444
carrot <caryl830@gmail.com>
2021-09-30 14:22:26 +0800

    Add start script to auth

---
+++ back-end/products/src/db/index.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+const { db_string } = require("../config");
+
+const connectToDB = () => {
+  mongoose.connect(db_string, {
+    dbName: "tektrek",
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+  var db = mongoose.connection;
+  db.on("error", console.error.bind(console, "Error connecting to MongoDB"));
+  db.once("open", function () {
+    console.log("Connected successfully");
+  });
+};
+
+module.exports = connectToDB;
